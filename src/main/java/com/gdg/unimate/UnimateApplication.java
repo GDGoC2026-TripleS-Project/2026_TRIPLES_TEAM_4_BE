b6401f25b94b4ffcd38c5a414188b0cba4e6537d
seokhawn01 <skhu202214139@gmail.com>
2026-01-13 20:19:12 +0900

    fix/#: 기본설정

--- src/main/java/com/gdg/unimate/UnimateApplication.java
+++ src/main/java/com/gdg/unimate/UnimateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnimateApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UnimateApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UnimateApplication.class, args);
+	}
 
 }
--- src/test/java/com/gdg/unimate/UnimateApplicationTests.java
+++ src/test/java/com/gdg/unimate/UnimateApplicationTests.java
@@ -3,11 +3,13 @@ package com.gdg.unimate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        classes = UnimateApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
 class UnimateApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
